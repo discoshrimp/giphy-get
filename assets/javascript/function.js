4c711f726c1523fb5c169ae4321b7bbf6284e0e1
discoshrimp <roydega@gmail.com>
2018-06-26 05:56:39 -0700

    setup and started js

---
+++ assets/javascript/function.js
@@ -0,0 +1,12 @@
+//button array
+var button=[]
+var searchTerm = $("#search-term").val()
+var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
+
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+  })
