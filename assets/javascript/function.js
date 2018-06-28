165adfaa9431d9ee0ba237ce5c6fdd337ec3b47d
discoshrimp <roydega@gmail.com>
2018-06-27 21:59:40 -0700

    added button creation, just need to link data to button

--- assets/javascript/function.js
+++ assets/javascript/function.js
@@ -1,12 +1,27 @@
 //button array
-var button=[]
-var searchTerm = $("#search-term").val()
-var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
+var button = [];
+var searchTerm = "";
+var queryURL = "";
+var apiKey = "&api_key=I82fvTvK3La6kptLUDQNW9TAyxizAT8E";
+$(document).ready(function () {
+  $("button").on("click", function () {
+    
+    var movieTitle = $("#search-term").val().trim();
 
+    searchTerm = movieTitle;
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    console.log(response);
+    queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + apiKey;
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      $("#new-button").append("<button class='btn btn-large btn-secondary'>"+searchTerm+"</button>");
+      console.log(response);
+    })
+
+    console.log(searchTerm);
+
+    console.log(queryURL);
   })
+}) 
