41fb05fdeed32afdf6732f0a83672d43cc81411e
discoshrimp <roydega@gmail.com>
2018-06-29 08:19:27 -0700

    yes

--- assets/javascript/function.js
+++ assets/javascript/function.js
@@ -1,8 +1,12 @@
 //button array
 var button = [];
-var searchTerm = "";
+var searchTerm;
 var queryURL = "";
 var apiKey = "&api_key=I82fvTvK3La6kptLUDQNW9TAyxizAT8E";
+function gifPrint(){
+  var newImg =$("<img>")
+  newImg.attr("src" +response.data.url)
+}
 $(document).ready(function () {
   $("button").on("click", function () {
     
@@ -12,16 +16,32 @@ $(document).ready(function () {
 
     queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + apiKey;
 
-    $.ajax({
+    if(searchTerm===""){
+      alert("Please enter a search term")
+    }else{
+      $("#new-button").append("<button class='btn btn-large btn-secondary' src='" +queryURL+ "'>" +searchTerm+ "</button>");
+
+      $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function (response) {
-      $("#new-button").append("<button class='btn btn-large btn-secondary'>"+searchTerm+"</button>");
+      button.push(queryURL);
       console.log(response);
     })
-
     console.log(searchTerm);
-
     console.log(queryURL);
+  }
   })
+$(".btn-secondary").on("click", function(){
+  gifPrint();
+})
 }) 
+
+
+//create a counting variable
+
+//use counter to distinguish different variables in buttona rray
+
+//push queryUrl to button array
+
+//call upon button
